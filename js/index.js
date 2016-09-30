fa3ee746a98b8b692a9753f0fcc4b66d6b0fecd8
anhh027 <798046494@qq.com>
2016-09-30 10:00:31 +0800

    9-30

--- js/index.js
+++ js/index.js
@@ -9,5 +9,51 @@ $(function(){
             })
         })
 
+//提交订单
+    var wig=$(".tjdd_weixin_img");
+    //aa.eq(0).addClass("dd");
+    wig.each(function(i){
+        wig.eq(i).click(function(){
+            wig.removeClass("mydd").eq(i).addClass("mydd");
+        })
+    })
+// 购物车
+    var spdj=$(".tjdd_item_je2").text();
+    //减少
+    $(".reduce_num").on('click',function(){
+        var amount = $(this).parent().find(".amount");
+        if (parseInt($(amount).text()) <= 1){
+            alert("商品数量最少为1");
+        } else{
+            $(amount).text(parseInt($(amount).text()) - 1);
+        }
+        //小计
+        //var spdj=parseInt($(".tjdd_item_je").text());
+        var subtotal = parseInt(spdj) * parseInt($(amount).text());
+        $(this).parent().parent().find(".tjdd_item_je .tjdd_item_je2").text(subtotal.toFixed(1));
+        //总计金额
+        var total = 0;
+        $(".tjdd_item_je .tjdd_item_je2").each(function(){
+            total += parseInt($(this).text());
+        });
+
+        $("#total").text(total.toFixed(1));
+    });
+
+    //增加
+    $(".add_num").on('click',function(){
+        var amount = $(this).parent().find(".amount");
+        $(amount).text(parseInt($(amount).text()) + 1);
+        //小计
+        var subtotal = parseInt(spdj) * parseInt($(amount).text());
+        $(this).parent().parent().find(".tjdd_item_je .tjdd_item_je2").text(subtotal.toFixed(1));
+        //总计金额
+        var total = 0;
+        $(".tjdd_item_je .tjdd_item_je2").each(function(){
+            total += parseInt($(this).text());
+        });
+
+        $("#total").text(total.toFixed(1));
+    });
    
 })
